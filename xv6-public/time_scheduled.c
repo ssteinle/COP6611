3d30a7f1c9ccec94c3f164e415cc69431db1c698
onurbil <onurbil@hotmail.com>
2023-02-12 16:00:22 -0500

    time_scheduled

--- xv6-public/time_scheduled.c
+++ xv6-public/time_scheduled.c
@@ -5,8 +5,20 @@
 int main(int argc, char *argv[]) {
 
 
+    if (argc < 2){
+        int calling_pid = getpid();
 
+        printf(1, "%d", calling_pid);
+        printf(1, "This is time_schedule() of calling function...\n");
+        return calling_pid;
+    }
+    else{
 
-        printf(1, "This is time_schedule()...\n");
-    	exit();
+        int pid = atoi(argv[1]);
+        int aa = uptime();
+        printf(1, "%d", pid);
+        printf(1, "%d", aa);
+        exit();
+
+    }
 }
