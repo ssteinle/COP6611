500503213bd1b32be052637fca5bf74106795263
Kaptain000 <92124400+Kaptain000@users.noreply.github.com>
2023-02-27 21:30:49 -0500

    Add files via upload

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -51,6 +51,7 @@ struct proc {
   char name[16];               // Process name (debugging)
   int ctime;
   int priority;
+  int rtime;
 };
 
 // Process memory is laid out contiguously, low addresses first:
