507f0e682d8ef30b2e022365625448b6ca030fb9
ssteinle <stephensteinle@gmail.com>
2023-03-02 23:00:38 -0500

    fluff 6

--- xv6-public/dummy.c
+++ xv6-public/dummy.c
@@ -14,24 +14,15 @@ int main(int argc, char *argv[]) {
   fork();
   fork();
   fork();
-  // for ( h = 0; h < g; h++ ) {
-  //   pid = fork ();
-  //   if ( pid < 0 ) {
-  //     printf(1, "not working \n");
-  //   } else if (pid > 0) {
-  //     printf(1, "p. %d creates c. %d\n",getpid(), pid);
-  //     wait();
-  //     }
-  //     else{
-	// printf(1,"c. %d made\n",getpid());
+
 	for(b = 0; b < 10000; b+=1)
-	    a = a + 259;
+		a = a + 259;
+
   printf(2, "\nMy pid: %d\n", getpid());
 	// break;
   //     }
   // }
   while(wait() != -1);
-
   exit();
 }
 
